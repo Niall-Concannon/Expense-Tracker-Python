2af7b9adbd5d8a6b2341798ff50bf98f340d2444
NIALL CONCANNON <nconcannon556@gmail.com>
2025-09-09 14:36:08 +0100

    Added for loop to print out keys and value pairs

--- expense_tracker.py
+++ expense_tracker.py
@@ -15,15 +15,21 @@ while more != "N":
     print("Enter more? Y or N: ")
     more = input()
 
-    if more == "Y":
+    if(more == "Y" or more == "y"):
         print("Y selected")
+
         # Add to dictionary
         expenses[category] = amount
     
-    else:
+    elif(more == "N" or more == "n"):
         print("N selected")
+
         # Add to dictionary
         expenses[category] = amount
         break
 
-print(expenses)
+print("\nPrinting out Expense List")
+
+# Loop to print out all dictionary items
+for key, value in expenses.items():
+    print("Category:", key, "Amount:", value)
