3edc9c46c7fd4def78beec4fa5bcda432eb9e950
NIALL CONCANNON <nconcannon556@gmail.com>
2025-09-09 14:22:23 +0100

    Added a loop to loop through user input

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,13 +1,29 @@
 # Create the dictionary
 expenses = { }
+category = ""
+amount = 0
+more = "Y"
 
-# Get user input
-print("Enter category: ")
-category = input()
+while more != "N":
+    # Get user input
+    print("Enter category: ")
+    category = input()
 
-print("Enter amount spent: ")
-amount = input()
+    print("Enter amount spent: ")
+    amount = input()
 
-# Add to dictionary
-expenses[category] = amount
+    print("Enter more? Y or N: ")
+    more = input()
+
+    if more == "Y":
+        print("Y selected")
+        # Add to dictionary
+        expenses[category] = amount
+    
+    else:
+        print("N selected")
+        # Add to dictionary
+        expenses[category] = amount
+        break
+
 print(expenses)
